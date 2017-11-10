ab999164c98c15f7db8a33fe1d05b56f6c0e02d4
Gaia94 <riccardigaia94@gmail.com>
2017-11-10 13:46:05 +0100

    bower update

--- _/js/app.js
+++ _/js/app.js
@@ -1,6 +1,16 @@
 angular
     .module('appStrangerThings', [])
     .controller('strangerThingsController', function ($scope) {
-        $scope.hello= 'hello world';
-        console.log('ciao');
+
+
+
+        $scope.letters = ["a", "b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
+        // $scope.press = function (e, i) {
+        //
+        //     socket.emit('message');
+        //     console.log(e, i);
+        // }
+
     });
