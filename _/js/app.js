7be04e9a832ee6d5adc61b3dfe9bc3ccdf518691
Gaia94 <riccardigaia94@gmail.com>
2017-11-10 15:14:36 +0100

    update socket

--- _/js/app.js
+++ _/js/app.js
@@ -2,15 +2,20 @@ angular
     .module('appStrangerThings', [])
     .controller('strangerThingsController', function ($scope) {
 
-
+        var socket = io.connect();
 
         $scope.letters = ["a", "b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 
-        // $scope.press = function (e, i) {
-        //
-        //     socket.emit('message');
-        //     console.log(e, i);
-        // }
+        $scope.pressLetter = function (i) {
+            socket.emit('message', i);
+            console.log(i, 'o');
+        };
+
+
+        socket.on('message', function (response) {
+            $scope.responseLetter = response;
+            console.log( $scope.responseLetter, 'respo');
+        })
 
     });
