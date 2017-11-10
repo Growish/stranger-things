5e790713e30505ed93a25247737ccfb1478fc02d
Gaia94 <riccardigaia94@gmail.com>
2017-11-10 12:53:20 +0100

    bower update

---
+++ _/js/app.js
@@ -0,0 +1,6 @@
+angular
+    .module('appStrangerThings', [])
+    .controller('strangerThingsController', function ($scope) {
+        $scope.hello= 'hello world';
+        console.log('ciao');
+    });
