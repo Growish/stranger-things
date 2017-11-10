1e1d4a8e4a2e029dcb8b6a39e4422ab6778ab4f5
Andrea <andrea.valzasina@growish.com>
2017-11-10 13:03:40 +0100

    bug fix

--- server.js
+++ server.js
@@ -4,13 +4,12 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-
-app.set('port', 3000);
-
 app.use('/', express.static(path.join(__dirname + '/_/')));
 
 io.on('connection', function(socket){
-    console.log('hello client!');
+    socket.on('message', function(msg){
+        socket.broadcast.emit('message', msg);
+    });
 });
 
 http.listen(3000, function(){
